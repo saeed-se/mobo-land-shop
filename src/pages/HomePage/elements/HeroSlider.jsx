f958f18897b53063baf25734800c39f1cebff341
Saeed <saeedranjbar2002@gmail.com>
2026-09-21 19:32:51 +0330

    fix vercel deployment

--- src/pages/HomePage/elements/HeroSlider.jsx
+++ src/pages/HomePage/elements/HeroSlider.jsx
@@ -7,6 +7,11 @@ import "swiper/css/pagination";
 import "swiper/css/navigation";
 import SliderButtons from "@/components/common/SliderButtons";
 
+import slider1 from "@/assets/slider-1.webp";
+import slider2 from "@/assets/slider-2.webp";
+import slider3 from "@/assets/slider-3.gif";
+import slider4 from "@/assets/slider-4.gif";
+
 const HeroSlider = () => {
   return (
     <div className="relative h-45 md:h-80 mt-10 md:mt-15">
@@ -24,32 +29,16 @@ const HeroSlider = () => {
         className="h-full"
       >
         <SwiperSlide>
-          <img
-            className="h-full"
-            src="/src/assets/slider-1.webp"
-            alt="slider-1"
-          />
+          <img className="h-full" src={slider1} alt="slider-1" />
         </SwiperSlide>
         <SwiperSlide>
-          <img
-            className="h-full"
-            src="/src/assets/slider-2.webp"
-            alt="slider-2"
-          />
+          <img className="h-full" src={slider2} alt="slider-2" />
         </SwiperSlide>
         <SwiperSlide>
-          <img
-            className="h-full"
-            src="/src/assets/slider-3.gif"
-            alt="slider-3"
-          />
+          <img className="h-full" src={slider3} alt="slider-3" />
         </SwiperSlide>
         <SwiperSlide>
-          <img
-            className="h-full"
-            src="/src/assets/slider-4.gif"
-            alt="slider-4"
-          />
+          <img className="h-full" src={slider4} alt="slider-4" />
         </SwiperSlide>
 
         <SliderButtons />
