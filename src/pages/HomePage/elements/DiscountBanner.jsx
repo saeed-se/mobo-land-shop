355e3ca876a3cecd9ed381d27c40d2bb8e330728
Saeed <saeedranjbar2002@gmail.com>
2026-09-21 20:44:11 +0330

    fix image path

--- src/pages/HomePage/elements/DiscountBanner.jsx
+++ src/pages/HomePage/elements/DiscountBanner.jsx
@@ -6,12 +6,7 @@ const DiscountBanner = () => {
   return (
     <div className="flex items-center flex-col gap-3 mt-5 xs:mt-0 xs:pr-7 md:pr-10">
       <img src={amazings} alt="amazings" width={150} height={150} />
-      <img
-        src="/src/assets/percentage.webp"
-        alt={percentage}
-        width={150}
-        height={150}
-      />
+      <img src={percentage} alt="percentage" width={150} height={150} />
       <HeroButton />
     </div>
   );
