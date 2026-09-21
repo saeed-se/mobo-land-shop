6b46142e4c1a1caac94952639ff48dd03c8d76dc
Saeed <saeedranjbar2002@gmail.com>
2026-09-21 20:38:07 +0330

    fix image paths and update home data

--- src/pages/HomePage/elements/DiscountBanner.jsx
+++ src/pages/HomePage/elements/DiscountBanner.jsx
@@ -1,17 +1,14 @@
 import HeroButton from "./HeroButton";
+import amazings from "@/assets/amazings.webp";
+import percentage from "@/assets/percentage.webp";
 
 const DiscountBanner = () => {
   return (
     <div className="flex items-center flex-col gap-3 mt-5 xs:mt-0 xs:pr-7 md:pr-10">
-      <img
-        src="/src/assets/amazings.webp"
-        alt="amazings"
-        width={150}
-        height={150}
-      />
+      <img src={amazings} alt="amazings" width={150} height={150} />
       <img
         src="/src/assets/percentage.webp"
-        alt="percentage"
+        alt={percentage}
         width={150}
         height={150}
       />
